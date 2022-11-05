a6827201ce88f43c43943c5425e1c4e99ab481a0
Alexander Celis <alexcelis019@gmail.com>
2022-11-05 11:15:30 -0500

    create types and resolvers

--- index.js
+++ index.js
@@ -6,21 +6,42 @@ export const persons = [
     firstName: 'Terry',
     lastName: 'Medhurst',
     phone: '+63 791 675 8914',
-    email: 'atuny0@sohu.com'
+    email: 'atuny0@sohu.com',
+    bank: {
+      cardExpire: '06/22',
+      cardNumber: '50380955204220685',
+      cardType: 'maestro',
+      currency: 'Peso',
+      iban: 'NO17 0695 2754 967'
+    }
   },
   {
     id: 2,
     firstName: 'Sheldon',
     lastName: 'Quigley',
     phone: '+7 813 117 7139',
-    email: 'hbingley1@plala.or.jp'
+    email: 'hbingley1@plala.or.jp',
+    bank: {
+      cardExpire: '10/23',
+      cardNumber: '5355920631952404',
+      cardType: 'mastercard',
+      currency: 'Ruble',
+      iban: 'MD63 L6YC 8YH4 QVQB XHIK MTML'
+    }
   },
   {
     id: 3,
     firstName: 'Terrill',
     lastName: 'Hills',
     phone: '+63 739 292 7942',
-    email: 'rshawe2@51.la'
+    email: 'rshawe2@51.la',
+    bank: {
+      cardExpire: '10/23',
+      cardNumber: '3586082982526703',
+      cardType: 'jcb',
+      currency: 'Peso',
+      iban: 'AT24 1095 9625 1434 9703'
+    }
   },
   {
     id: 4,
@@ -41,24 +62,48 @@ export const persons = [
     firstName: 'Mavis',
     lastName: 'Schultz',
     phone: '+372 285 771 1911',
-    email: 'kmeus4@upenn.edu'
+    email: 'kmeus4@upenn.edu',
+    bank: {
+      cardExpire: '01/24',
+      cardNumber: '4917245076693618',
+      cardType: 'visa-electron',
+      currency: 'Euro',
+      iban: 'IT41 T114 5127 716J RGYB ZRUX DSJ'
+    }
   }
 ]
 
 /* Definición de los datos */
 const typeDefs = gql`
-type Person {
-  id: ID!
-  firstName: String!
-  lastName: String!
-  phone: String
-  email: String!
-}
+  type Bank {
+    cardExpire: String!
+    cardNumber: String!
+    cardType: String!
+    currency: String!
+    iban: String!
+  }
 
-type Query {
-  personCount: Int!
-  allPersons: [Person]!
-}
+  type FullNameObj {
+    name: String!
+    last: String!
+  }
+
+  type Person {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    phone: String
+    email: String!
+    bank: Bank!
+    fullName: FullNameObj!
+    enterName: String!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person]!
+    findPerson(name: String!): Person
+  }
 `
 
 /* Resolvers */
@@ -66,7 +111,15 @@ type Query {
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons
+    allPersons: () => persons,
+    findPerson: (root, args) => {
+      const { name } = args
+      return persons.find((person) => person.firstName === name)
+    }
+  },
+  Person: {
+    enterName: (root) => `${root.firstName} ${root.lastName}`,
+    fullName: (root) => ({ name: root.firstName, last: root.lastName })
   }
 }
 
